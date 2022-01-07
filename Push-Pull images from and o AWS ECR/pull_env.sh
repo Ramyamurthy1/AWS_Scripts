33a5055686d36bdb5a900f0581b6cefdbd3d4476
ramyamurthy1 <ramyamurthy1@gmail.com>
2022-01-07 12:49:35 +0530

    new scripts

---
+++ Push-Pull images from and o AWS ECR/pull_env.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034,SC2154
+# ------------------------------------------------------------------------------
+#  PURPOSE:       PULL ECR IMAGES from the AWS ACCOUNT recursively
+# ------------------------------------------------------------------------------
+#  PREREQS: a) 
+#           b) 
+#           c) 
+#           d)
+# ------------------------------------------------------------------------------
+#  EXECUTE: scripts/get-creds.sh -v --env foo -z bar --project baz
+# ------------------------------------------------------------------------------
+#     TODO: 1) 
+#           2) 
+#           3) 
+# ------------------------------------------------------------------------------
+#   AUTHOR: Todd E Thomas
+# ------------------------------------------------------------------------------
+#  CREATED: 2018/10/00
+# ------------------------------------------------------------------------------
+#set -x
+
+
+###-----------------------------------------------------------------------------
+### VARIABLES
+###-----------------------------------------------------------------------------
+
+
+#!/bin/bash
+#SOURCE ENVIRONMENT Variables
+source env-ecr
+
+:"$AWS_ACCOUNT_ID_SRC?        No Source Account ID defined}"
+:"$AWS_REGION_SRC?            No Source region  defined}"
+:"$TARGET_ENV?                No Target Environment specified}" 
+
+echo $src
+echo $dst
+
+
+cp $HELM_PATH/composer-values.yaml $HELM_PATH/composer-values-$TARGET_ENV.yaml
+sed -i -r "s#^(    registryIp: ).*#\1${dst//#/\\#}#"  composer-values-$TARGET_ENV.yaml
+
+
+
+echo "Logging INTO SOURCE"
+$(aws ecr get-login --no-include-email --region $AWS_REGION_SRC)
+aws ecr get-login-password --region $AWS_REGION_SRC | docker login --username AWS --password-stdin $src
+echo "LOGIN TO SOURCE SUCCESSFUl"
+
+
+
+
+sed -n 's/^.*\([Ii]mage\)/\1/p' $HELM_PATH/composer-values.yaml > out.txt
+sed 's/image://g' out.txt > a.txt
+sed -i '/imagePullPolicy:/d' a.txt
+sed '/^$/d' a.txt > image_list.txt
+sed -i -- 's/imageName: / /g' image_list.txt
+sed -i -- 's/Image: / /g' image_list.txt
+
+
+
+
+
+echo $TARGET_ENV
+TAG=$line-$TARGET_ENV
+echo "TAG",$TAG
+
+while read line
+do
+  echo "image" $line
+  echo "============"
+  docker pull $src/$line
+  docker tag $src/$line $dst/$line-$TARGET_ENV
+  echo $line-$TARGET_ENV >> test.txt
+  sed -i "s/$line/$line-$TARGET_ENV/" composer-values-$TARGET_ENV.yaml
+done < image_list.txt
+
+
---
+++ Push-Pull images from and o AWS ECR/push_env.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034,SC2154
+# ------------------------------------------------------------------------------
+#  PURPOSE:      
+# ------------------------------------------------------------------------------
+#  PREREQS: a) 
+#           b) 
+#           c) 
+#           d)
+# ------------------------------------------------------------------------------
+#  EXECUTE: 
+# ------------------------------------------------------------------------------
+#     TODO: 1) 
+# ------------------------------------------------------------------------------
+#   AUTHOR: Ramya Murthy
+# ------------------------------------------------------------------------------
+#  CREATED: 2021/10/
+# ------------------------------------------------------------------------------
+#set -x
+
+# Script to push the ECR images to ECR REP on WT1
+
+#!/bin/bash
+source env-ecr
+
+require_env() {
+  if [[ -z "${!1}" ]]; then
+    echo "Require Env.$1... Not Found"
+    exit 1
+  fi
+  echo Require Env.$1... OK "(${!1})"
+}
+
+require_env AWS_ACCOUNT_ID_SRC
+require_env AWS_ACCOUNT_ID_DST
+require_env AWS_REGION_SRC
+require_env AWS_REGION_DST
+require_env TARGET_ENV
+ 
+echo $src
+echo $dst
+
+echo $TARGET_ENV
+TAG=$line-$TARGET_ENV
+echo "TAG",$TAG
+
+
+aws ecr get-login-password --region $AWS_REGION_DST | docker login --username AWS --password-stdin $dst
+
+while read line
+do
+  echo "image" $line
+  docker push $dst/$line-$TARGET_ENV
+done < image_list.txt
+aws s3 cp composer-values-$TARGET_ENV.yaml s3://cicd-deploy-test-wt1/ibm-sc/composer-values-$TARGET_ENV.yaml
+
