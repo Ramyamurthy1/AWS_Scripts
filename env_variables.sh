37fc131c6a7ab1428189d6748813dbe3b3726677
Ramyamurthy1 <71689655+Ramyamurthy1@users.noreply.github.com>
2022-04-28 14:55:57 +0530

    Create env_variables.sh

---
+++ env_variables.sh
@@ -0,0 +1,7 @@
+
+BUCKET_NAME="ibm-dish-sourcecode"
+TEST_DATA_BUCKET="ibm-dish-sourcecode"
+RESULTS_BUCKET="ibm-test-data-cicd-wt2"
+CURRENT_PATH=`pwd`
+CODE_REPO_NAME="COMPOSER-Release-0.22.13"
+EKS_ENV_NAME="dish-test"
