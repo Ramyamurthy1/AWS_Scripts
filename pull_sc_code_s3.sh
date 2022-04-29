5dbc8d7b4672b02810fcb1fa25f17007eac76c6f
Ramyamurthy1 <71689655+Ramyamurthy1@users.noreply.github.com>
2022-04-29 12:11:43 +0530

    Update pull_sc_code_s3.sh

--- pull_sc_code_s3.sh
+++ pull_sc_code_s3.sh
@@ -1,10 +1,34 @@
-########################################
-#######################################
-# 1. Pull Code from S3
-# 2. Run Auutomated script
-# 3. Upload the results to s3
-###########################################
-##########################################
+
+#####################################################################
+# 1. Pull Code from S3                                              #
+# 2. Copy the test data file from s3 in case not present in github  #
+# 3. Run Automated script                                           #
+# 4. View depenedency results file                                  #
+# 5. Upload the results to s3                                       #
+# 6. Clean up the dir by removing the logs, jsons and csv file      #
+# ################################################################### 
+
+
+
+###########################################################################################
+# PRE-REQUISITES                                                                          #
+# Latest Code must be present in S3 bucket under the BUCKET name and not within sub dirs  #
+# env_Variables file is updated with BUCKET information                                   #
+# "token_url" file fie is updated with the correct URL'S                                  #
+# #########################################################################################
+
+
+#################################################################################################
+#  STEPS to Run the Tests                                                                       #
+#  ========================                                                                     #
+#  1. Clone the s3_pull_code dir to the local folder where you want to run the tests            #
+#  2. Update env_variables file with bucket information                                         #
+#  3. Update the token_url file ith latest URLS according to the env                            #
+#  4. Run "./pull_test_code.sh"                                                                 #
+#  5. Select the required option                                                                #
+##################################################################################################
+
+
 source env_variables
 
 require_env() {
@@ -51,7 +75,7 @@ function s3_bucket_check {
 
 echo $BUCKET_NAME
 echo $TEST_DATA_BUCKET
-echo $RESULTS_BUCKE
+echo $RESULTS_BUCKET
 echo "*********************************"
 
 s3_bucket_check $BUCKET_NAME
@@ -59,7 +83,7 @@ s3_bucket_check $TEST_DATA_BUCKET
 s3_bucket_check $RESULTS_BUCKET
 
 while true; do
-     echo  -e "Enter the action to be performed. \n USAGE: \n a - To Pull the code only \n b - To Copy test data excel file from S3 \n c - To Run Automated Test \n d - To view depenedency results file  \n e. Upload Results/Logs/Jsons to S3 \n f. To Exit"
+     echo  -e "Enter the action to be performed. \n USAGE: \n a - To Pull the code only \n b - To Copy test data excel file from S3 \n c - To Run Automated Test \n d - To view depenedency results file  \n e -  Upload Results/Logs/Jsons to S3 \n f -  To Exit"
      echo "<><><><>><><><><><><><><><><><><><><><><><><><><><><><><>"
 
      read -p 'Enter action to be performed : ' 'user_action'
@@ -67,24 +91,32 @@ while true; do
 
 
      if [[ $user_action == "a" ]]; then
+             echo  -e "You have Selected to Copy the Latest code from S3 \n"
              read -p 'Enter the Test Automation Code Repo name : ' 'CODE_REPO_NAME'
              echo $CODE_REPO_NAME
              aws s3 cp s3://${BUCKET_NAME}/$CODE_REPO_NAME ./
              tar -xvf  $CODE_REPO_NAME.tar
-             echo "Code download is completed"
+             cp ./get_token.py $CODE_REPO_NAME/TestAutomation/Code/all_rfss/
+             echo "Code download is completed.  The tokens are copied."
 
      elif [[ $user_action == "b" ]];then
+             echo  -e "You have Selected to Copy test data deom S3 bucket \n"
              read -p 'Enter the file name to be copied: ' 'DATA_FILE'
              echo $DATA_FILE
+
              #aws s3 ls s3://$TEST_DATA_BUCKET
              TEST_DATA_PATH=${CODE_REPO_NAME}/TestAutomation/TestData/${EKS_ENV_NAME}
 
              aws s3 cp s3://$TEST_DATA_BUCKET/$DATA_FILE ${CURRENT_PATH}/${TEST_DATA_PATH}
-             echo "Download complete \n"
+             echo -e "Download complete \n"
+             echo "====================================================================================================="
 
 
      elif [[ $user_action == "c" ]]; then
              #read -p 'Enter the code repo release : ' 'CODE_REPO_NAME'
+             echo  -e "You have Selected to run the automated tests \n"
+             echo "Folowing files Exist. Select the fie you want to run"
+             ls -lrt ${CODE_REPO_NAME}/TestAutomation/TestData/${EKS_ENV_NAME}
              read -p 'Enter the file name for test execution : ' 'DATA_FILE'
              echo $CODE_REPO_NAME
              echo ${DATA_FILE}
@@ -103,22 +135,28 @@ while true; do
 
 
      elif [[ $user_action == "d" ]]; then
-             cd ${CODE_REPO_NAME}/TestAutomation/Code/all_rfss/
+             echo  -e "You have Selected to view the dependency results file generated. \n"
+             cd ${CURRENT_PATH}/${CODE_REPO_NAME}/TestAutomation/Code/all_rfss/
              dependency_file=`ls ${CURRENT_PATH}/${CODE_REPO_NAME}/TestAutomation/Code/all_rfss/dependency* | sort -t _ -k 2,2 | tail -n 1`
              echo $dependency_file
+             echo -e "=============================================================================================================================\n"
              cat $dependency_file
+             echo -e "============================================================================================================================="
 
 
      elif [[ $user_action == "e" ]]; then
+             echo  -e "You have Selected to Copy the Test Results, Test Logs and TMF orders to S3. \n"
              echo "Copying test Results, Logs and JSONs to ", $RESULTS_BUCKET
+             cd $CURRENT_PATH
              ./push_s3.sh
 
 
 
      elif [[ $user_action == "f" ]]; then
+             echo  -e "You have Selected to exit. \n"
              exit
      else
-             echo "Nothing was entered correctly"
+             echo "Nothing was entered correctly. Select correct option"
              exit
      fi
  done
